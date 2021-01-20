d641100c9c7ef24ce8748974c6bee60280e91642
WELL1NGTON <wellingtonmassola@gmail.com>
2021-01-20 14:46:06 -0300

    Asserções (Assert)

--- TestesDeSoftware/01 - Testes de Unidade/1.1 - Testes Basicos/Demo/Calculadora.cs
+++ TestesDeSoftware/01 - Testes de Unidade/1.1 - Testes Basicos/Demo/Calculadora.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Demo
 {
     public class Calculadora
@@ -9,6 +11,12 @@ namespace Demo
 
         public double Dividir(double v1, double v2)
         {
+            var result = v1 / v2;
+
+            //Dividing a floating-point value by zero doesn't throw an exception; it results in positive infinity, negative infinity, or not a number (NaN)
+            //https://docs.microsoft.com/en-us/dotnet/api/system.dividebyzeroexception?redirectedfrom=MSDN&view=net-5.0
+            if (double.IsInfinity(result)) throw new DivideByZeroException();
+
             return v1 / v2;
         }
     }
