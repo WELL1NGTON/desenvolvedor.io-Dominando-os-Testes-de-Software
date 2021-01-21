71b207341940ac97bdef35abadfd2fb7e894b74f
WELL1NGTON <wellingtonmassola@gmail.com>
2021-01-21 11:25:30 -0300

    Pedido - Atualizar Item

--- TestesDeSoftware/01 - Testes de Unidade/2.2 - Features/Features.Tests/08 - Skip/TesteNaoPassandoMotivoEspecifico.cs
+++ TestesDeSoftware/01 - Testes de Unidade/2.2 - Features/Features.Tests/08 - Skip/TesteNaoPassandoMotivoEspecifico.cs
@@ -4,7 +4,7 @@ namespace Features.Tests
 {
     public class TesteNaoPassandoMotivoEspecifico
     {
-        [Fact(DisplayName = "Novo Cliente 2.0", Skip = "Nova versão 2.0 quebrando")]
+        [Fact(Skip = "Nova versão 2.0 quebrando")]
         [Trait("Categoria", "Escapando dos Testes")]
         public void Teste_NaoEstaPassando_VersaoNovaNaoCompativel()
         {
